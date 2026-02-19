27b31b6a3e27924ec284a3af0f2063dd83172268
Fabio R. Medeiros <fabiorm85@gmail.com>
2026-02-19 09:48:00 -0300

    fix: ajustments on CNPJ validation

--- Api/Core/ValueObjects/ClientCnpj.cs
+++ Api/Core/ValueObjects/ClientCnpj.cs
@@ -10,12 +10,12 @@ public record ClientCnpj
     
     public ClientCnpj(string cnpj)
     {
-        string normalizedCnpj = Regex.Replace(cnpj.Trim(), @"[^0-9]", "");
+        string normalizedCnpj = Regex.Replace(cnpj, @"[./-]", "").Trim();
         
         if (string.IsNullOrWhiteSpace(normalizedCnpj)) 
             throw new ArgumentException("CNPJ value cannot be null or empty.", nameof(normalizedCnpj));
         
-        if (cnpj.Length != MaxLength)
+        if (normalizedCnpj.Length != MaxLength)
             throw new ArgumentException($"Invalid CNPJ length: {normalizedCnpj.Length}");
         
         if (!normalizedCnpj.All(c => char.IsDigit(c)))
