8d1159b169154dfb1fb7b953436581f4e8d32869
Fabio R. Medeiros <fabiorm85@gmail.com>
2026-03-02 22:08:22 -0300

    fix: fix MediatR service injection

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,14 +1,26 @@
+using System.Reflection;
+using Application.Common.Behaviors;
 using Infrastructure.Persistence;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseNpgsql(
-                builder.Configuration.GetConnectionString("DefaultConnection"),
-                x => x.MigrationsAssembly("Infrastructure")
-                )
-        );
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
+        configuration.GetConnectionString("DefaultConnection"),
+        x => x.MigrationsAssembly("Infrastructure")
+    )
+);
+
+services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+services.AddScoped(
+    typeof(IPipelineBehavior<,>),
+    typeof(UnitOfWorkBehavior<,>));
 
 var app = builder.Build();
 
